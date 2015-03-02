784168b017c8315140c8073d99124cdc25ed6d93
NikolayKapustin <jetrok@yandex.ru>
2015-03-03 00:49:24 +0300

    add fork

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 /**
  * Created by nikolay on 07.02.15.
  */
-var express = require('express'),
-  bodyParser = require('body-parser'),
-  init = require('./init'),
-  fileUpload = false,
-  app = express();
-global.__home = __dirname;
+var cluster = require('cluster');
 
-app.use('/', express.static(__dirname + '/public'));
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(bodyParser());
-init(app);
-app.listen(8080);
+if (cluster.isMaster) {
+  cluster.fork();
 
-var getToken = 'https://oauth.vk.com/authorize?client_id=4771476&redirect_uri=https://oauth.vk.com/blank.html&scope=notify,friends,status,wall,groups,messages,notifications,stats,offline&display=page&response_type=token';
+  cluster.on('listening', function(worker, address) {
+    console.warn('Worker %d is now listening on port %d',
+      worker.process.pid, address.port);
+  });
+
+  cluster.on('exit', function(worker) {
+    console.warn('Worker %d died.', worker.process.pid);
+    cluster.fork();
+  });
+} else {
+  require('./constructor')();
+}
---
+++ constructor.js
@@ -0,0 +1,20 @@
+/**
+ * Created by nikolay on 03.03.15.
+ */
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  init = require('./init'),
+  app = express();
+
+var getToken = 'https://oauth.vk.com/authorize?client_id=4771476&redirect_uri=https://oauth.vk.com/blank.html&scope=notify,friends,status,wall,groups,messages,notifications,stats,offline&display=page&response_type=token';
+
+global.__home = __dirname;
+
+module.exports = function() {
+  app.listen(8080);
+  app.use('/', express.static(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(bodyParser());
+  init(app);
+};
